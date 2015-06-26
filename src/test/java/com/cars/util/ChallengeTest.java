2cafb8abefa70d23cbc4e7da139d49bb42b6730b
Keith Kreissl <kkreissl@cars.com>
2015-06-26 12:34:13 -0500

    moved answer to another folder

--- src/test/java/com/cars/util/ChallengeTest.java
+++ src/test/java/com/cars/util/ChallengeTest.java
@@ -9,10 +9,8 @@ public class ChallengeTest {
 	
 	@Test
 	public void testPalindrome() throws Exception{
-		
 		assertTrue(new Challenge().isPalindrome("racecar"));
 		assertFalse(new Challenge().isPalindrome("cars.com"));
-		
 	}
 
 }
